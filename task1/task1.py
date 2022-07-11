2a06c5228dd3297a7f457623cf7e89b721732c5e
Bartek Wit <bartlomiej.wit@gmail.com>
2022-07-11 15:31:26 +0200

    pierwszy probny

---
+++ task1/task1.py
@@ -0,0 +1,13 @@
+import requests
+import xml.etree.ElementTree as ET
+
+
+def task1_solution():
+    r = requests.get('https://coding-academy.pl/all_customers')
+    return r
+
+if __name__ == '__main__':
+    root = ET.fromstring(task1_solution().content)
+    pl_tst = open("test.txt", "w")
+    for child in root:
+        pl_tst.write(child.text + '\n')
